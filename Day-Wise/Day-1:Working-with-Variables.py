33853531508a1ba53f8c0d3b29f8719d87f519d5
Aneesh Ajay Akut <135816561+aneeshakut@users.noreply.github.com>
2024-01-22 14:20:29 +0530

    Create Day-1:Working-with-Variables.py

---
+++ Day-Wise/Day-1:Working-with-Variables.py
@@ -0,0 +1,5 @@
+#Band Name Generator
+print("Welcome to Band Name Generator.")
+City = input("What's the name of city you grew up in?")
+Pet = input("What's your pet's name?")
+print("Your band name could be",City,Pet)
