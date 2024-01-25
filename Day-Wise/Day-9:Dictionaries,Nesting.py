51fe1215e8077bda7039a8feaf0395561b843be3
Aneesh Ajay Akut <135816561+aneeshakut@users.noreply.github.com>
2024-01-25 19:24:53 +0530

    Create Day-9:Dictionaries,Nesting.py

---
+++ Day-Wise/Day-9:Dictionaries,Nesting.py
@@ -0,0 +1,27 @@
+#Auction Program
+
+import os
+clear_screen = os.system('cls')
+
+bids = {}
+bidding_finished = False
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"Winner is {winner} with a bid of {highest_bid}")
+
+while not bidding_finished:
+    name = input("What's your name? ")
+    price = float(input("What is your bid? $"))
+    bids[name] = price
+    should_continue = input("Are there any more bidders?Type 'yes' or 'no' ")
+    if should_continue == "no":
+        bidding_finished = True
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        pass
