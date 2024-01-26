6afa2855cdd4829679a7629df4e9c657961b85a5
Aneesh Ajay Akut <135816561+aneeshakut@users.noreply.github.com>
2024-01-26 20:31:51 +0530

    Create Day-13:Debugging.py

---
+++ Day 11-20/Day-13:Debugging.py
@@ -0,0 +1,13 @@
+#Debugging
+
+pages = 0
+words_per_page = 0
+pages = int(input("Number of pages:"))
+words_per_page = int(input("Words per page:"))
+total_words = pages * words_per_page
+
+#Printing out values to confirm them
+print(f"pages = {pages}")
+print(f"words per page = {words_per_page}")
+
+print(total_words)
